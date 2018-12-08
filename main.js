878d566a817f9462fc82af4cc2d8cc6137f93422
alanoudssr <alanoudssr@gmail.com>
2018-12-08 16:39:15 +0300

    fixed mobile nav

--- main.js
+++ main.js
@@ -34,3 +34,12 @@ $('a[href*="#"]')
             }
         }
     });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
